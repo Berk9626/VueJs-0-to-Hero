b77d111f0248f4e6048b2744b706e9db3905ac73
berkberkmen12 <berkberkmen12@gmail.com>
2024-11-09 17:27:31 +0300

    updated 9.11.2024

---
+++ my-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<app-header/>
+<div class="container">
+  
+    <h3>Bu benim ilk vue cli</h3>
+    <p>xD</p>
+    
+    
+    
+</div>
+
+<Counteritem></Counteritem>
+
+
+
+ 
+</template>
+
+<script>
+import Counteritem from '@/components/CounterItem.vue';
+
+
+export default {
+  
+  components: {
+    Counteritem,
+  }
+
+}
+
+</script>
+
+
+<style>
+
+</style>
---
+++ my-project/src/components/CounterItem.vue
@@ -0,0 +1,27 @@
+ <template>
+     <div class="app">
+      <div class="container-sm">
+        <h3 class="mb-2"> {{counter}} </h3>
+
+        <button @click="counter--" class="red sm">-</button>
+        <button @click="counter++" class="green sm">+</button>
+      </div>
+    </div>
+ </template>
+
+
+<script>
+export default {
+
+    data(){
+        return{
+            counter: 0,
+        }
+    }
+
+ }
+
+ </script>
+ 
+ 
+ 
